4ffc42e523d1b505cdbf2b9695e1d6af08b82aff
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-03-23 14:17:40 +0530

    If- Else in JavaScript

---
+++ 05 If-Else/main.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "Kshitiz",
+    age: "18",
+    isMarried: false
+}
+
+if(person.isMarried){
+    console.log(person.firstName + " is Married.");
+} else {
+    console.log(person.firstName + " is not Married.");
+}
